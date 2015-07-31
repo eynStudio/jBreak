e8c35b1e9fce4ce702263b564f9203aa9cddf092
pswang <pengshengwang@gmail.com>
2015-07-31 15:53:29 +0800

    重构：直接用express为服务器，不再使用angular-mocks

--- src/docs/docs.js
+++ src/docs/docs.js
@@ -1,4 +1,4 @@
-angular.module('jbdemo', ['ngRoute', 'jb.ui', 'ngSanitize', 'jb.ctx', 'demo.tpls', 'e2e-mocks', 'ui.router', 'jb.zd'])
+angular.module('jbdemo', ['ngRoute', 'jb.ui', 'ngSanitize', 'jb.ctx', 'demo.tpls', 'ui.router', 'jb.zd'])
     .factory('phoneCtx',function(jbCtxP){
         var ctx=jbCtxP('/api/phones/:id',{ id: '@id' });
 
